6dc2337d0e9ebc17c67cc3a3fad0a2189cf7d56c
Afzal <sah.afzal@gmail.com>
2020-01-12 19:53:19 +0500

    Read me added

--- app.js
+++ app.js
@@ -7,29 +7,44 @@ var vm = new Vue({
     },
     methods: {
         drawShape: function () {
+            // Check for not allowing user to voilate padding rule
             if (this.padding % 2 !== 0 || this.padding < 4 ||this.padding > 60) {
                 alert (`Padding value should be even and greater or equal to 4 and less than or equal 60, ${this.padding} given`)
                 return;
             }
+
+            // Check for not allowing user to voilate height rule
             if (this.height % 2 !== 0  || this.height < 20 || this.height > 300) {
                 alert(`Height value should be even and greater or equal to 20 and less than or equal 300 ${this.height} given`)
                 return;
             }
+
+            // Check for not allowing user to voilate width rule
             if (this.width % 2 !== 0  || this.width < 20 || this.width > 300) {
-                alert(`Height value should be even and greater or equal to 20 and less than or equal 300 ${this.width} given`)
+                alert(`Width value should be even and greater or equal to 20 and less than or equal 300 ${this.width} given`)
                 return;
             }
-            const getShapeCordinates = this.makeShape(this.width, this.height , this.padding);
+
+            // this metod gives you the cordinates 2D array
+            const pixelArray  = this.makeShape(this.width, this.height , this.padding);
+
+            // we used pre selector for diplaying shapes
             const output = document.querySelector("pre");
-            output.textContent = getShapeCordinates.map(row => row.map(i => " -|"[i]).join``).join`\n`;
+
+            // the final output of shapes goes from this line by looping all rows
+            output.textContent = pixelArray .map(row => row.map(i => " -|"[i]).join``).join`\n`;
 
         },
+
+        // this method is used for not allowing user to do input string and non-numeric values in height, width and padding
         onlyNumber ($event) {
             let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
             if ((keyCode < 48 || keyCode > 57) || keyCode === 46) { // 46 is dot
                $event.preventDefault();
             }
         },
+
+        // recursive method for making shapes row
         makeShape : function (width, height, padding) {
             if (width <= padding || height <= padding) {
                 if (width <= 0 || height <= 0) return [];
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(favicon(__dirname + '/logo.png'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname)));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
+
+// this is for delivering app on heroku
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
