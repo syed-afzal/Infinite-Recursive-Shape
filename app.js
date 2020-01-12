36a12e04cd5e9fe7b6a9e4988a994accab117d34
Afzal <sah.afzal@gmail.com>
2020-01-12 17:57:54 +0500

    innitial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        width:20,
+        height:20,
+        padding:4
+    },
+    methods: {
+        drawShape: function () {
+            if (this.padding % 2 !== 0 || this.padding < 4 ||this.padding > 60) {
+                alert (`Padding value should be even and greater or equal to 4 and less than or equal 60, ${this.padding} given`)
+                return;
+            }
+            if (this.height % 2 !== 0  || this.height < 20 || this.height > 300) {
+                alert(`Height value should be even and greater or equal to 20 and less than or equal 300 ${this.height} given`)
+                return;
+            }
+            if (this.width % 2 !== 0  || this.width < 20 || this.width > 300) {
+                alert(`Height value should be even and greater or equal to 20 and less than or equal 300 ${this.width} given`)
+                return;
+            }
+            const getShapeCordinates = this.makeShape(this.width, this.height , this.padding);
+            const output = document.querySelector("pre");
+            output.textContent = getShapeCordinates.map(row => row.map(i => " -|"[i]).join``).join`\n`;
+
+        },
+        onlyNumber ($event) {
+            let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
+            if ((keyCode < 48 || keyCode > 57) || keyCode === 46) { // 46 is dot
+               $event.preventDefault();
+            }
+        },
+        makeShape : function (width, height, padding) {
+            if (width <= padding || height <= padding) {
+                if (width <= 0 || height <= 0) return [];
+                if (height < 2) return [Array(width).fill(1)];
+                return [
+                  Array(width).fill(1),
+                  ...Array.from({length: height-2}, () => width < 2 ? [2] : [2, ...Array(width-2).fill(0), 2]),
+                  Array(width).fill(1),
+                ];
+              }
+              return [
+                Array(width).fill(1),
+                ...Array.from({length: padding>>1}, () => [2, ...Array(width-2).fill(0), 2]),
+                ...this.makeShape(width - padding - 2, height - padding - 2, padding).map((row,i) =>
+                  [2, ...Array(padding>>1).fill(0), ...row, ...Array(padding>>1).fill(0), 2]
+                ),
+                ...Array.from({length: padding>>1}, () => [2, ...Array(width-2).fill(0), 2]),
+                Array(width).fill(1)
+              ];
+        }
+    }
+});
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 3003;
+const app = express();
+app.use(favicon(__dirname + '/logo.png'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'index.html')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.listen(port, () => {
+    console.log("Server is up on port", port)
+});
